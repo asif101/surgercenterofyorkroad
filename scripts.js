53127988ad72e546a2edf8339ae0d56d14edbd16
Joseph Conroy <joey.conroy@jhuapl.edu>
2020-06-03 13:51:14 -0400

    added alert

--- scripts.js
+++ scripts.js
@@ -1,25 +1,26 @@
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+//Dismiss alert banner
+function dismissAlert() {
+  let alert = document.getElementById('covid-alert');
+  alert.style.display = 'none';
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+//Show statement modal
+function showStatement() {
+  let statement = document.getElementById('statement-modal');
+  statement.classList.remove("closed");
+}
+function closeStatement() {
+  let statement = document.getElementById('statement-modal');
+  statement.classList.add("closed");
 }
 
-// When the user clicks anywhere outside of the modal, close it
+//Dismiss modal when clicking outside content
+let statementContainer = document.getElementById('statement-modal');
+let statementContent = document.getElementById('statement-content');
+let learnMoreButton = document.getElementById('covid-button');
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if ((event.target != statementContent) && (event.target != learnMoreButton)) {
+    statementContainer.classList.add("closed");
   }
 }
